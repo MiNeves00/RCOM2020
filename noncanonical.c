35ec9705bc7c502fbfb20c36b9a2913ccee1c344
Zé Miguel Maçães <56728035+zemmacaes@users.noreply.github.com>
2020-11-02 18:31:03 +0000

    Allows S4

--- noncanonical.c
+++ noncanonical.c
@@ -78,7 +78,8 @@ int main(int argc, char **argv)
       ((strcmp("/dev/ttyS10", argv[1]) != 0) &&
        (strcmp("/dev/ttyS11", argv[1]) != 0) &&
        (strcmp("/dev/ttyS0", argv[1]) != 0) &&
-       (strcmp("/dev/ttyS1", argv[1]) != 0)))
+       (strcmp("/dev/ttyS1", argv[1]) != 0) &&
+       (strcmp("/dev/ttyS4", argv[1]) != 0)))
   {
     printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS11\n");
     exit(1);
